4c6335c5b9f8d53f80f829cb5d413cac1e16b8a8
SubwayMan <tyche4490@gmail.com>
2022-04-11 18:31:19 -0700

    converted consumers to async

--- testing/consumers.py
+++ testing/consumers.py
@@ -1,29 +1,27 @@
 import json
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class CanvasConsumer(WebsocketConsumer):
-    def connect(self):
+class CanvasConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_name = "canvas"
 
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_name,
             self.channel_name
         )
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
             self.room_name,
             self.channel_name
         )
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         pixel = text_data_json['position']
         color = text_data_json['color']
-        print(pixel, color)
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_name,
             {
                 'type': 'pixel_event',
@@ -32,8 +30,8 @@ class CanvasConsumer(WebsocketConsumer):
             }
         )
     
-    def pixel_event(self, event):
-        self.send(text_data=json.dumps({
+    async def pixel_event(self, event):
+        await self.send(text_data=json.dumps({
             "position": event["position"],
             "color": event["color"]
         }))
