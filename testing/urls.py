ca22c10361a6533d9bc892916fc30a5b14d64e6a
SubwayMan <tyche4490@gmail.com>
2022-04-21 22:55:09 -0700

    got website icon to work

--- testing/urls.py
+++ testing/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
 from .views import API
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 
 urlpatterns = [
@@ -10,5 +12,4 @@ urlpatterns = [
     path('ajax/canvas', views.ajax_get_canvas),
     path('ajax/update', views.ajax_update_canvas),
     path('ajax/login', views.ajax_validate_user),
-
 ]
